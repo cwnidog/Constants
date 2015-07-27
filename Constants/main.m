8d07f177a5cb2699a6f5c860b517a5e9d5224b43
cwnidog <cwnidog@comcast.net>
2015-07-27 16:16:41 -0700

    Adding code to demo various constants-related topics

---
+++ Constants/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by John Leonard on 7/27/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char *argv[])
+{
+  @autoreleasepool
+  {
+    NSLog(@"\u03c0 is %f", M_PI);
+  } // @autoreleasepool
+    return 0;
+} // main()
